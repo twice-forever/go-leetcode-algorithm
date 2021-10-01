05cceac80f121c77851d4e7c9478e89d4ad81e66
twice-forever <bnn0202@outlook.com>
2021-10-01 14:01:30 +0800

    feat: 改进算法

--- problem/reverseinteger.go
+++ problem/reverseinteger.go
@@ -1,39 +1,15 @@
 package problem
 
-import (
-	"strconv"
-)
-
 func Reverse(x int) int {
-	negative := []byte("-")[0]
-	s := strconv.Itoa(x)
-	sLen := len(s)
-	sIndex := sLen - 1
-	newByte := make([]byte, 0, sLen)
-	if s[0] == negative {
-		newByte = append(newByte, negative)
+	n := 0
+
+	for x != 0 {
+		n = n*10 + x%10
+		x = x / 10
 	}
 
-	for {
-		if s[sIndex] == negative {
-			break
-		}
-
-		newByte = append(newByte, s[sIndex])
-		sIndex--
-
-		if sIndex == -1 {
-			break
-		}
-	}
-
-	newS := string(newByte)
-	newI, err := strconv.Atoi(newS)
-	if err != nil {
+	if int(int32(n)) != n {
 		return 0
 	}
-	if int(int32(newI)) != newI {
-		return 0
-	}
-	return newI
+	return n
 }
