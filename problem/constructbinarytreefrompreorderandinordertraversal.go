7dc1259e1fdb68f310d7bc2b7e9153486ad2001b
twice-forever <bnn0202@outlook.com>
2021-10-18 22:35:01 +0800

    feat: 从前序与中序遍历序列构造二叉树 修改算法

--- problem/constructbinarytreefrompreorderandinordertraversal.go
+++ problem/constructbinarytreefrompreorderandinordertraversal.go
@@ -1,32 +1,20 @@
 package problem
 
 func BuildTree(preorder []int, inorder []int) *TreeNode {
-	treeNode := &TreeNode{}
-	rebuilder(preorder, inorder, treeNode)
-	return treeNode
-}
-
-func rebuilder(preorder []int, inorder []int, node *TreeNode) {
 	if len(preorder) == 0 {
-		node = nil
-		return
+		return nil
 	}
 
-	node.Val = preorder[0]
+	treeNode := &TreeNode{
+		Val: preorder[0],
+	}
+
 	for i, v := range inorder {
-		if v == node.Val {
-			leftInorder := inorder[:i]
-			rightInorder := inorder[i+1:]
-			leftPreorder := preorder[1 : len(leftInorder)+1]
-			rightPreorder := preorder[len(leftInorder)+1:]
-			if len(leftPreorder) > 0 {
-				node.Left = &TreeNode{}
-				rebuilder(leftPreorder, leftInorder, node.Left)
-			}
-			if len(rightPreorder) > 0 {
-				node.Right = &TreeNode{}
-				rebuilder(rightPreorder, rightInorder, node.Right)
-			}
+		if v == treeNode.Val {
+			treeNode.Left = BuildTree(preorder[1:len(inorder[0:i])+1], inorder[:i])
+			treeNode.Right = BuildTree(preorder[len(inorder[0:i])+1:], inorder[i+1:])
+			break
 		}
 	}
+	return treeNode
 }
