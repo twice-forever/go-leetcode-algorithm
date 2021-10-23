957726a0bb4bb1f66f442c453519f8a1e39a98b7
twice-forever <bnn0202@outlook.com>
2021-10-23 21:46:09 +0800

    feat: 二叉树的最小深度

---
+++ problem/mindepthbittree.go
@@ -0,0 +1,20 @@
+package problem
+
+import "math"
+
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	if root.Left == nil && root.Right == nil {
+		return 1
+	}
+	minD := math.MaxInt32
+	if root.Left != nil {
+		minD = min(minDepth(root.Left), minD)
+	}
+	if root.Right != nil {
+		minD = min(minDepth(root.Right), minD)
+	}
+	return minD + 1
+}
