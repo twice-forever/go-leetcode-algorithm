8c9eb3fdf683c46f11e1ac3aa3159bf263f05bd3
twice-forever <bnn0202@outlook.com>
2021-11-03 22:34:09 +0800

    feat: 改进结构

--- problem/binarytreepaths.go
+++ problem/binarytreepaths.go
@@ -3,19 +3,16 @@ package problem
 import "fmt"
 
 func binaryTreePaths(root *TreeNode) []string {
-	var strSlice []string
-	if root.Left != nil {
-		strSlice = binaryTreePaths(root.Left)
+	if root == nil {
+		return []string{}
 	}
-	if root.Right != nil {
-		strSlice = append(strSlice, binaryTreePaths(root.Right)...)
-	}
+	strSlice := binaryTreePaths(root.Left)
+	strSlice = append(strSlice, binaryTreePaths(root.Right)...)
 	for i := 0; i < len(strSlice); i++ {
 		strSlice[i] = fmt.Sprintf("%d->%s", root.Val, strSlice[i])
 	}
 	if root.Right == nil && root.Left == nil {
 		strSlice = append(strSlice, fmt.Sprintf("%d", root.Val))
 	}
-
 	return strSlice
 }
