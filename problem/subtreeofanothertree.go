5e4b76ff98fe74f16f5aa1161b050e8f2dd9ad6d
twice-forever <bnn0202@outlook.com>
2021-11-17 22:32:24 +0800

    feat: 另一棵树的子树

---
+++ problem/subtreeofanothertree.go
@@ -0,0 +1,34 @@
+package problem
+
+func isSubtree(root *TreeNode, subRoot *TreeNode) bool {
+	if root == nil && subRoot == nil {
+		return true
+	}
+	if subRoot == nil || root == nil {
+		return false
+	}
+	leftEqual := isSubtree(root.Left, subRoot)
+	rightEqual := isSubtree(root.Right, subRoot)
+	if leftEqual || rightEqual {
+		return true
+	}
+	if root.Val == subRoot.Val {
+		return equalVal(root, subRoot)
+	}
+	return false
+}
+
+func equalVal(root, subRoot *TreeNode) bool {
+	if root == nil && subRoot == nil {
+		return true
+	}
+	if subRoot == nil || root == nil {
+		return false
+	}
+	if root.Val != subRoot.Val {
+		return false
+	}
+	leftEqual := equalVal(root.Left, subRoot.Left)
+	rightEqual := equalVal(root.Right, subRoot.Right)
+	return leftEqual && rightEqual
+}
