9c90d9571a88dce1055d99b83f2310fbe9582578
twice-forever <bnn0202@outlook.com>
2021-11-21 18:54:09 +0800

    feat: 官方解答

--- problem/findduplicatesubtrees.go
+++ problem/findduplicatesubtrees.go
@@ -1,57 +1,24 @@
 package problem
 
+import "fmt"
+
 func findDuplicateSubtrees(root *TreeNode) []*TreeNode {
-	subTree := []*TreeNode{}
-	var getSubTree func(root *TreeNode)
-	getSubTree = func(root *TreeNode) {
-		if root.Left != nil {
-			subTree = append(subTree, root.Left)
-			getSubTree(root.Left)
+	nodeMap := map[string]int{}
+	nodes := []*TreeNode{}
+	var makeStr func(node *TreeNode) string
+	makeStr = func(node *TreeNode) string {
+		if node == nil {
+			return "#"
 		}
-		if root.Right != nil {
-			subTree = append(subTree, root.Right)
-			getSubTree(root.Right)
+		left := makeStr(node.Left)
+		right := makeStr(node.Right)
+		str := fmt.Sprintf("%d,%s,%s", node.Val, left, right)
+		nodeMap[str]++
+		if nodeMap[str] == 2 {
+			nodes = append(nodes, node)
 		}
+		return str
 	}
-	getSubTree(root)
-
-	sameSubTree := []*TreeNode{}
-	for len(subTree) > 1 {
-		str := subTree[0]
-		subTree = subTree[1:]
-		hasSame := false
-		for _, v := range sameSubTree {
-			if compareTree(str, v) {
-				hasSame = true
-				break
-			}
-		}
-		if !hasSame {
-			for j := 0; j < len(subTree); j++ {
-				if compareTree(str, subTree[j]) {
-					sameSubTree = append(sameSubTree, str)
-					break
-				}
-			}
-		}
-	}
-	return sameSubTree
-}
-
-func compareTree(src, target *TreeNode) bool {
-	if src == nil && target == nil {
-		return true
-	}
-	if src == nil || target == nil {
-		return false
-	}
-	if src.Val != target.Val {
-		return false
-	}
-	left := compareTree(src.Left, target.Left)
-	right := compareTree(src.Right, target.Right)
-	if left == true && right == true {
-		return true
-	}
-	return false
+	makeStr(root)
+	return nodes
 }
