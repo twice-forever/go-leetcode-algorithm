163b22388f18a07c14ca0327d84c1a9d68e1286a
twice-forever <bnn0202@outlook.com>
2021-11-27 17:11:53 +0800

    feat: 二叉搜索树中的搜索

---
+++ problem/searchinabinarysearchtree.go
@@ -0,0 +1,14 @@
+package problem
+
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val == val {
+		return root
+	}
+	if root.Val > val {
+		return searchBST(root.Left, val)
+	}
+	return searchBST(root.Right, val)
+}
