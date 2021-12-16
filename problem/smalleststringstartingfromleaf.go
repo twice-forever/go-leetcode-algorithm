41877a3a5444641553709864c818e91ea6f20b0f
twice-forever <bnn0202@outlook.com>
2021-12-16 21:31:10 +0800

    feat: 从叶结点开始的最小字符串

---
+++ problem/smalleststringstartingfromleaf.go
@@ -0,0 +1,31 @@
+package problem
+
+func smallestFromLeaf(root *TreeNode) string {
+	var ans string
+	var dfs func(node *TreeNode, words string)
+	dfs = func(node *TreeNode, words string) {
+		if node == nil {
+			return
+		}
+		word := byte('a' + node.Val)
+		words = string(append([]byte{word}, []byte(words)...))
+		if node.Left == nil && node.Right == nil {
+			if ans == "" {
+				ans = words
+				return
+			}
+			for i := 0; i < len(ans); i++ {
+				if i == len(words) || words[i] < ans[i] {
+					ans = words
+					break
+				} else if ans[i] < words[i] {
+					break
+				}
+			}
+		}
+		dfs(node.Left, words)
+		dfs(node.Right, words)
+	}
+	dfs(root, "")
+	return ans
+}
