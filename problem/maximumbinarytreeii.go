51209214f26f004276e87193a8492c681a1a8a58
twice-forever <bnn0202@outlook.com>
2021-12-18 11:02:13 +0800

    feat: 更简洁的写法

--- problem/maximumbinarytreeii.go
+++ problem/maximumbinarytreeii.go
@@ -1,37 +1,12 @@
 package problem
 
 func insertIntoMaxTree(root *TreeNode, val int) *TreeNode {
-	var nums []int
-	var maxNum = 0
-	var dfs func(node *TreeNode)
-	dfs = func(node *TreeNode) {
-		if node == nil {
-			return
-		}
-		dfs(node.Left)
-		if maxNum < node.Val {
-			maxNum = node.Val
-		}
-		nums = append(nums, node.Val)
-		dfs(node.Right)
+	if root == nil {
+		return &TreeNode{Val: val}
 	}
-	dfs(root)
-	nums = append(nums, val)
-	return buildTree(nums)
-}
-
-func buildTree(nums []int) *TreeNode {
-	if len(nums) == 0 {
-		return nil
+	if root.Val < val {
+		return &TreeNode{Val: val, Left: root}
 	}
-	var maxI, maxV int
-	for i, v := range nums {
-		if maxV < v {
-			maxI, maxV = i, v
-		}
-	}
-	root := &TreeNode{Val: maxV}
-	root.Left = buildTree(nums[:maxI])
-	root.Right = buildTree(nums[maxI+1:])
+	root.Right = insertIntoMaxTree(root.Right, val)
 	return root
 }
