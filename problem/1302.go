48014943332ce444f8c03a7053899a3e05da53ec
twice-forever <bnn0202@outlook.com>
2021-12-30 21:16:20 +0800

    feat: 层数最深叶子节点的和2

--- problem/1302.go
+++ problem/1302.go
@@ -1,21 +1,23 @@
 package problem
 
 func deepestLeavesSum(root *TreeNode) int {
-	nums := 0
-	nodes := []*TreeNode{root}
-	for len(nodes) > 0 {
-		nums = 0
-		tmpNodes := []*TreeNode{}
-		for _, v := range nodes {
-			nums += v.Val
-			if v.Left != nil {
-				tmpNodes = append(tmpNodes, v.Left)
-			}
-			if v.Right != nil {
-				tmpNodes = append(tmpNodes, v.Right)
+	depth, sum := 0, 0
+	var dfs func(node *TreeNode, deep int)
+	dfs = func(node *TreeNode, deep int) {
+		if node == nil {
+			return
+		}
+		if node.Left == nil && node.Right == nil {
+			if depth < deep {
+				depth = deep
+				sum = node.Val
+			} else if depth == deep {
+				sum += node.Val
 			}
 		}
-		nodes = tmpNodes
+		dfs(node.Left, deep+1)
+		dfs(node.Right, deep+1)
 	}
-	return nums
+	dfs(root, 0)
+	return sum
 }
