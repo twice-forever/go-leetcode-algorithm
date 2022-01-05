34ba76668245f84572ea93b3970005a238113489
twice-forever <bnn0202@outlook.com>
2022-01-05 22:14:41 +0800

    feat: 替换所有的问号

---
+++ problem/1576.go
@@ -0,0 +1,17 @@
+package problem
+
+func ModifyString(s string) string {
+	res := []byte(s)
+	n := len(res)
+	for i, ch := range res {
+		if ch == '?' {
+			for b := byte('a'); b <= 'c'; b++ {
+				if !(i > 0 && res[i-1] == b || i < n-1 && res[i+1] == b) {
+					res[i] = b
+					break
+				}
+			}
+		}
+	}
+	return string(res)
+}
