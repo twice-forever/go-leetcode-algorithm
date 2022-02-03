437ff241c7f48f56421a5ebc119f7e1608a96bed
twice-forever <bnn0202@outlook.com>
2022-02-03 16:42:40 +0800

    feat: 打家劫舍 II

--- problem/213.go
+++ problem/213.go
@@ -3,9 +3,22 @@ package problem
 func Robiii(nums []int) int {
 	n := len(nums)
 	if n > 1 {
-		n1 := robii(nums[:n-1])
-		n2 := robii(nums[1:n])
-		return max(n1, n2)
+		return max(count(nums[:n-1]), count(nums[1:n]))
 	}
 	return nums[0]
+}
+
+func count(nums []int) int {
+	l := len(nums)
+	if l == 0 {
+		return 0
+	}
+	if l == 1 {
+		return nums[0]
+	}
+	f, s := nums[0], max(nums[0], nums[1])
+	for i := 2; i < l; i++ {
+		f, s = s, max(f+nums[i], s)
+	}
+	return s
 }
